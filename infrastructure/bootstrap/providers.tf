e7a0eba13ee68617c57348ab12d7adf94719ceb3
RuariW12 <ruariw16@gmail.com>
2026-04-19 14:39:46 -0400

    reconfigured providers.tf

--- infrastructure/bootstrap/providers.tf
+++ infrastructure/bootstrap/providers.tf
@@ -1,4 +1,14 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-  version = "~> 3.0"
-  region  = us-east-2
+  region = var.region
 }
