e3f8789c46c72ec72afda219bc7f79bf596b17ef
RuariW12 <ruariw16@gmail.com>
2026-04-22 11:27:07 -0400

    configured rds outputs

---
+++ infrastructure/modules/rds/outputs.tf
@@ -0,0 +1,9 @@
+output "db_instance_endpoint" {
+  description = "RDS instance endpoint"
+  value       = module.rds.db_instance_endpoint
+}
+
+output "db_credentials_secret_arn" {
+  description = "ARN of the Secrets Manager secret containing DB credentials"
+  value       = aws_secretsmanager_secret.db_credentials.arn
+}
