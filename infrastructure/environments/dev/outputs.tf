f46a3c81620d9d0730f81a8209a7a2ccc7333988
RuariW12 <ruariw16@gmail.com>
2026-04-22 12:19:12 -0400

    configured outputs for dev env

---
+++ infrastructure/environments/dev/outputs.tf
@@ -0,0 +1,19 @@
+output "cloudfront_domain_name" {
+  description = "URL to access the application"
+  value       = module.cdn.cloudfront_domain_name
+}
+
+output "alb_dns_name" {
+  description = "ALB DNS name for direct API access"
+  value       = module.alb.alb_dns_name
+}
+
+output "db_instance_endpoint" {
+  description = "RDS endpoint"
+  value       = module.rds.db_instance_endpoint
+}
+
+output "frontend_bucket_name" {
+  description = "S3 bucket for frontend deployment"
+  value       = module.cdn.frontend_bucket_name
+}
